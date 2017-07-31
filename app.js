31f1c1a6a7c9c7a5cdf276283703df9c236fe521
Alice Chen <alicejychen15@gmail.com>
2017-07-31 18:28:34 -0400

    Added middleware funtion

--- app.js
+++ app.js
@@ -5,10 +5,19 @@ app.listen(3000, function() {
   console.log('server listening');
 });
 
+app.use(function(req, res, next){
+  console.log(req.method, req.url, res.statusCode);
+  next();
+});
+
 app.get('/', function(req,res,next) {
   res.send('hey');
 });
 
 app.get('/news', function(req,res,next) {
   res.send('no news');
+});
+
+app.get('*', function(req, res, next){
+  res.send('we can handle it all')
 });
