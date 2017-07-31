72b0d8a2d9aa3b2117039f81e9bf10b161e72588
Mieka Page <git@coldwine.nyc>
2017-07-31 18:03:05 -0400

    server listens and responds

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-node_module
+const express = require('express');
+const app = express();
+
+app.listen(3000, function() {
+  console.log('server listening');
+});
+
+app.get('/', function(req,res,next) {
+  res.send('hi');
+})
