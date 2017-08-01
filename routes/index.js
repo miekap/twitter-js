6ccb7833cdf105dba19b4667f50fc21664ce8bb3
Alice Chen <alicejychen15@gmail.com>
2017-08-01 12:17:27 -0400

    made a live thing

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
-// const express = require('express');
-// const router = express.Router();
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
+//const router = require('express').Router();
 const tweetBank = require('../tweetBank');
 const path = require('path');
 
@@ -8,8 +8,10 @@ router.get('/', function (req, res) {
   let tweets = tweetBank.list();
   res.render( 'index', { tweets: tweets } );
 });
-router.get('/stylesheets/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, '../public/stylesheets/style.css'));
-});
+// gets the stylesheet
+// router.get('/stylesheets/style.css', function (req, res) {
+//   res.sendFile(path.join(__dirname, '../public/stylesheets/style.css'));
+// });
+router.use(express.static('public'));
 
 module.exports = router;
