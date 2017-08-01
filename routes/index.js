247502413961ad0797071705de1d2c94391d1349
Alice Chen <alicejychen15@gmail.com>
2017-08-01 13:01:08 -0400

    added user pages

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,13 @@ router.get('/', function (req, res) {
   let tweets = tweetBank.list();
   res.render( 'index', { tweets: tweets } );
 });
+
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var list = tweetBank.find( {name: name} );
+  res.render( 'index', { tweets: list } );
+});
+
 // gets the stylesheet
 // router.get('/stylesheets/style.css', function (req, res) {
 //   res.sendFile(path.join(__dirname, '../public/stylesheets/style.css'));
